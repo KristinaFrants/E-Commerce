0cd63d531574ef66ae18f19ea47508bafcb6c939
myname <cristina.chiticari@gmail.com>
2020-10-20 16:26:57 -0400

    navbar

--- src/Header.js
+++ src/Header.js
@@ -2,21 +2,57 @@ import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
         <nav className="header bg-dark">
-            {/* Logo on the left */}
-            <Link to="/">
-        <img className= "header__logo m-1" src="https://library.kissclipart.com/20180921/pre/kissclipart-ecommerce-website-icon-png-clipart-e-commerce-webs-c27adca43148f9e8.png"/>
+
+        {/* Logo on the left */}
+        <Link to="/">
+            <img className= "header__logo m-1" src="https://library.kissclipart.com/20180921/pre/kissclipart-ecommerce-website-icon-png-clipart-e-commerce-webs-c27adca43148f9e8.png"/>
         </Link>
-            {/* Search box */}
+
+         {/* Search box */}
            <div className="header__search">
-            <input type="text" className="header__searchInput"/>
-            <FontAwesomeIcon icon={faSearch} className="header__searchIcon"/>
+                <input type="text" className="header__searchInput"/>
+                <FontAwesomeIcon icon={faSearch} className="header__searchIcon"/>
             </div>
+
             {/* Links */}
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Enter</span>
+                    <span className="header__optionLineTwo">Sing In</span>
+                </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">Orders</span>
+                </div>
+                </Link>
+
+                <Link to="/" className="header__link">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+                </Link>
+
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        {/* Shopping basket icon */}
+                        <FontAwesomeIcon icon={faShoppingBasket}/>
+                        {/* Number of items in the basket */}
+                        <span className="header__optionLineTwo 
+                        header__basketCount">0</span>
+                    </div>
+                </Link>
+
+            </div>
             {/* Basket Icon */}
         </nav>
     )
