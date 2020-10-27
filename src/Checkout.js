08b45bc2c08bed94f5f41d45b4cfae7d43678374
myname <cristina.chiticari@gmail.com>
2020-10-27 14:08:18 -0400

    deployed

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,16 +5,18 @@ import CheckoutProduct from './CheckoutProduct';
 import {useStateValue} from "./StateProvider";
 
 function Checkout() {
-    const [{ basket }, dispath] = useStateValue();
+    const [{ basket, user }, dispath] = useStateValue();
 
     return (
         <div className="row checkout">
             <div className="checkout__left col-sm-8">
                 <img className="checkout__ad" src="https://image.freepik.com/free-vector/abstract-pink-polygon-banner-background_1035-19018.jpg"/>
                 <div>
+                <h6 className="ml-3"> Hello, {user?.email}</h6>
                 <h2 className="checkout__title">
                     Your Shopping Basket
                 </h2>
+                
                 {/* BasketItems */}
                     {basket.map(item =>(
                         <CheckoutProduct
