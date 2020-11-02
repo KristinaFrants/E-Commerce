42965c4d7aefc0fa61f36a37fa270a9d6a7c7e24
myname <cristina.chiticari@gmail.com>
2020-11-02 16:02:48 -0500

    db

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-8625e/us-central1/api" //API  cloud URL
+    baseURL:"https://us-central1-clone-8625e.cloudfunctions.net/api"
+     //"http://localhost:5001/clone-8625e/us-central1/api" //API  cloud URL
 });
 
 export default instance;
